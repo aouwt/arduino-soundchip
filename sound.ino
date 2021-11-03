0697950f54f05133aa0adca5187899e70e6ef88f
Kit <74026992+aouwt@users.noreply.github.com>
2021-11-03 16:08:44 -0700

    Update sound.ino

--- sound.ino
+++ sound.ino
@@ -1,30 +1,34 @@
 #define get_freq(freq) (1000000 / freq)
-#define CHANNELS 1
-typedef int freq;
+#define CHANNELS 10
+#define PIN 12
+typedef int freqtype;
 typedef unsigned long long time;
 
 struct channel {
   time next;
-  freq freq;
+  freqtype freq;
+  bool cycle;
 };
 
-channel Ch[CHANNELS];
+channel Ch[CHANNELS] = {0};
 
 void setup (void) {
-  pinMode (13, OUTPUT);
+  pinMode (PIN, OUTPUT);
   Ch[0].freq = get_freq(440);
-  Ch[1].freq = get_freq(523.25);
+  Ch[1].freq = get_freq(572.25);
 }
-
 bool last_state = false;
 time now;
 
 void loop (void) {
   now = micros ();
   for (char i = 0; i != CHANNELS; i++) {
-    if (Ch[i].next <= now) {
-      digitalWrite (13, (last_state = !last_state));
-      Ch[i].next = now + Ch[i].freq;
+    if (Ch[i].freq) {
+      digitalWrite (PIN, Ch[i].cycle);
+      if (Ch[i].next <= now) {
+        Ch[i].cycle = !Ch[i].cycle;
+        Ch[i].next = now + Ch[i].freq;
+      }
     }
   }
 }

834fc995d0ad739234dfe15fd372951710fa764e
Kit <74026992+aouwt@users.noreply.github.com>
2021-11-09 06:36:56 -0800

    Update sound2.ino

--- sound2.ino
+++ sound2.ino
@@ -12,7 +12,7 @@ const Song::song_t playMe[] = {
 void setup() {
   // put your setup code here, to run once:
   sound.init (10, 13);
-  sound.Ch[0].interval = freqToInterval(440);
+  //sound.Ch[0].interval = freqToInterval(440);
   song.init (&sound);
   song.playSong (playMe);
 }

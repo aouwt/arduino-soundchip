9f764a33862ac13aed60c30a9efa509fa7793c1c
Kit <74026992+aouwt@users.noreply.github.com>
2021-11-12 19:43:08 -0500

    Update Song.h

--- Song/Song.h
+++ Song/Song.h
@@ -15,17 +15,13 @@
 #define SONG_END -3
 #define SONG_LOOP -4
 
-#define TICK_NORMAL 0
-#define TICK_FULL 1
-#define TICK_NORMALEQ 2
-#define TICK_FULLEQ 3
 
 #define stToFreq(note) (powf(2, (float)((note) - 49.0) / 12.0) * 440.0)
 #define stToInterval(note) (freqToInterval(stToFreq((note))))
 
 class Song {
   public:
-    typedef unsigned int time_t;
+    typedef unsigned long long time_t;
 
     typedef struct song_t {
       int8_t cmd;
@@ -43,14 +39,14 @@ class Song {
     void __attribute__((always_inline)) tick (void);
     void setTickType (char tickType);
     void end (void);
+    void clearChs (void);
 	
 
   private:
     time_t _now, _nextTime;
-    unsigned char _curPos = -1;
+    size_t _curPos = 0;
     unsigned int _curDelay = 0;
     Sound* _Sound;
-    void (Sound::*_tickType) (void); //dont really understand this but /shrug
 
 };
 #endif

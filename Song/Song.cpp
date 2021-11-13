3a7168d490c22e85da5f2deadab95370c27d1371
Kit <74026992+aouwt@users.noreply.github.com>
2021-11-12 19:43:22 -0500

    Update Song.cpp

--- Song/Song.cpp
+++ Song/Song.cpp
@@ -4,9 +4,8 @@ void Song::songTick (void) {
   if (isEnded) return;
   
   _now = millis ();
-  if (_now >= _nextTime) {;
-    _curPos++;
-    
+  if (_now >= _nextTime) {
+  
     if (curSong[_curPos].cmd >= 0) {
       _Sound -> ch[curSong[_curPos].cmd].interval = curSong[_curPos].arg;
     }
@@ -32,9 +31,10 @@ void Song::songTick (void) {
 	  break;
 
       }
-      return;
+      _curPos++; return;
     }
     _nextTime = _now + _curDelay;
+    _curPos++;
   }
 }
 
@@ -43,14 +43,14 @@ void Song::manualBegin (Sound* sound) {
 }
 
 void Song::begin (Sound::channelid_t chs, Sound::pin_t pin) {
-  _Sound = new Sound;
+  _Sound = (Sound*) malloc (sizeof(Sound));
   _Sound -> begin (chs, pin);
   setTickType (TICK_FULL);
 }
 
 void Song::end (void) {
   _Sound -> end ();
-  delete _Sound;
+  free (_Sound);
 }
 
 void Song::playSong (const Song::song_t* song) {
@@ -60,27 +60,21 @@ void Song::playSong (const Song::song_t* song) {
 
 void Song::restart (void) {
   isEnded = false;
-  curPos = 0;
+  _nextTime = 0;
+  _curPos = -1;
+  _curDelay = 0;
+  clearChs ();
+}
+
+void Song::clearChs (void) {
+  for (Sound::channelid_t i = 0; i != _Sound -> numOfChs; i++) {
+    _Sound -> ch[i].cycle = false;
+    _Sound -> ch[i].next = 0;
+    _Sound -> ch[i].interval = 0;
+  }
 }
 
 void __attribute__((always_inline)) Song::tick (void) {
   songTick ();
-  (_Sound ->* _tickType) (); //function pointers are fun
-}
-
-void Song::setTickType (char tickType) {
-  switch (tickType) {
-    case TICK_NORMAL:
-      _tickType = &_Sound -> tick;
-      break;
-    case TICK_FULL:
-      _tickType = &_Sound -> fullTick;
-      break;
-    case TICK_NORMALEQ:
-      _tickType = &_Sound -> tickEQ;
-      break;
-    case TICK_FULLEQ:
-      _tickType = &_Sound -> fullTickEQ;
-      break;
-  }
+  _Sound -> tick ();
 }
